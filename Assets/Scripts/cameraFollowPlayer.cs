1a7ebb5ea032515b0b282398f8421ecfb0c5b5a9
Roman Garms <38168462+Romano-Garmez@users.noreply.github.com>
2023-02-03 17:15:44 -0800

    initial demo

---
+++ Assets/Scripts/cameraFollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollowPlayer : MonoBehaviour {
+
+    public GameObject target;
+    public Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = target.transform.position + offset;
+	}
+}
